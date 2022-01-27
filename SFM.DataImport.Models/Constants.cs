3bd95c16f7f219d3da9110d4c7c955cbad7b499a
Eirik Søreng <eirik.soreng@nhn.no>
2022-01-27 12:11:40 +0100

    Adding missing Models files project

---
+++ SFM.DataImport.Models/Constants.cs
@@ -0,0 +1,35 @@
+namespace SFM.DataImport.Models
+{
+    public class Constants
+    {
+        /// <summary>
+        /// The name of the index file 
+        /// </summary>
+        public const string LedgerFilename = ".ledger";
+
+        /// <summary>
+        /// The name of the file containing organization data
+        /// </summary>
+        public const string OrganizationFilename = "ExportData.dat";
+
+        /// <summary>
+        /// The subfolder where patient files are stored
+        /// </summary>
+        public const string PatientFolder = "Patients";
+
+        /// <summary>
+        /// The subfolder where report files are stored
+        /// </summary>
+        public const string ReportFolder = "Reports";
+        
+        /// <summary>
+        /// The name of the organization XML file
+        /// </summary>
+        public const string OrganizationXmlFilename = "ExportData.xml";
+
+        /// <summary>
+        /// The name of the patient XML file
+        /// </summary>
+        public const string PatientXmlFilename = "PatientData.xml";
+    }
+}
---
+++ SFM.DataImport.Models/FileType.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel;
+
+namespace SFM.DataImport.Models
+{
+    /// <summary>
+    /// The file types accepted by the migration process
+    /// <remarks>This needs to be kept the same as SFM.Server.Core.DataImport.FileType</remarks>
+    /// </summary>
+    public enum FileType
+    {
+        [Description("Virksomhets fil")]
+        OrganizationFile,
+        [Description("Pasients fil")]
+        PatientFile,
+        [Description("Raport fil")]
+        ReportFile,
+        [Description("Ukjænt fil")]
+        Unknown,
+    }
+}
